5880f8c87481883db6765141aa91ddee96687eb7
Henry Owens <henry.tom.owens@gmail.com>
2021-10-12 13:46:09 +0200

    Remaking & hiding db secrets

--- src/config/db.ts
+++ src/config/db.ts
@@ -9,7 +9,7 @@ const connectDB = async () => {
       useUnifiedTopology: true,
     };
     await connect(
-      "mongodb+srv://admin:U3SChQwwrEIYqI9u@social-point.tlkcf.mongodb.net/users?retryWrites=true&w=majority",
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@social-point.tlkcf.mongodb.net/users?retryWrites=true&w=majority`,
       options
     );
     console.log("MongoDB Connected...");
--- src/starter.ts
+++ src/starter.ts
@@ -1,10 +1,14 @@
 import { json, urlencoded } from "body-parser";
 import express from "express";
+import { config } from "dotenv";
 
 import { routes } from "./api/routes";
 import { connectDB } from "./config/db";
 
 export function buildApp() {
+  // setting up env vars
+  config({ path: "./.env.local" });
+
   // create express server
   const app = express();
 
